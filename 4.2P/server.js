5eec5201ebd26563402c8ceaff3e0b54ba4c836c
April Dae Bation <adaecb@gmail.com>
2025-04-02 13:39:55 +1100

    Add connection to mongodb

--- 4.2P/server.js
+++ 4.2P/server.js
@@ -1,29 +1,24 @@
 var express = require("express");
 var app = express();
+const mongoose = require("mongoose");
 
+// Middleware
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+mongoose
+    .connect("mongodb://localhost:27017/myProjectDB")
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.error("MongoDB connection error:", err));
+
+const Project = require("./public/models/Projects");
 var port = process.env.port || 3000;
 
-const cardList = [
-    {
-        title: "Kitten 2",
-        image: "images/kitten-2.jpg",
-        link: "About Kitten 2",
-        description: "Demo description about Kitten 2",
-    },
-    {
-        title: "Kitten 3",
-        image: "images/kitten-3.jpg",
-        link: "About Kitten 3",
-        description: "Demo description about Kitten 3",
-    },
-];
-
-app.get("/api/projects", (req, res) => {
-    res.json({ statusCode: 200, data: cardList, message: "Success" });
+// REST
+app.get("/api/projects", async (req, res) => {
+    const projects = await Project.find({});
+    res.json({ statusCode: 200, data: projects, message: "Success" });
 });
 
 app.listen(port, () => {
